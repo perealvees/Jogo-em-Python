17c481f59bbfbe3e935be91c5191b97921a17e23
Priscilla Alves <perealvees@gmail.com>
2021-07-18 22:15:02 -0300

    Variáveis e operadores

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,6 +1,21 @@
-print('___________________________________')
-print('Bem vindo ao Jogo de Adivinhação')
-print('___________________________________')
+print('____________________________________')
+print('|                                  |')
+print('| Bem vindo ao Jogo de Adivinhação |')
+print('|__________________________________|\n')
 
-numero_secreto=42
+numero_secreto = 42
 
+chute_str = input('Digite um número: ')
+print('Você digitou', chute_str)
+
+chute = int(chute_str)
+
+if(numero_secreto == chute):
+    print('Acertô miseravi!')
+else:
+    if(chute > numero_secreto):
+        print('Você errou! Seu chute foi maior que o número secreto.')
+    elif(chute < numero_secreto):
+        print('Você errou! Seu chute foi menor que o número secreto.')
+
+print('Fim de jogo')
