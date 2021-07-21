05c1da00f57516dd7587b415578c952363589f90
Priscilla Alves <perealvees@gmail.com>
2021-07-21 14:13:02 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,24 +5,32 @@ print('|__________________________________|\n')
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-  while (rodada <= total_de_tentativas):
-    print('Tentativa', rodada, 'de',  total_de_tentativas)
-    chute_str = input('Digite um número: ')
-    print('Você digitou ', chute_str)
+
+for rodada in range (1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou " , chute_str)
     chute = int(chute_str)
 
-    acertou = chute == numero_secreto
-    maior   = chute >  numero_secreto
-    menor   = chute <  numero_secreto
+    if (chute < 1 or chute >100 ):
+        print('Você deve digitar um numero entre 1 e 100!')
+        continue
 
-    if(acertou):
-        print('Acertô miseravi!')
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Acertô miseravi!")
+        break
     else:
-        if(maior):
-            print('Você errou! Seu chute foi maior que o número secreto.')
-        elif(menor):
-            print('Você errou! Seu chute foi maior que o número secreto.')
+        if (maior):
+            print("O seu chute foi maior do que o número secreto!")
+        elif (menor):
+            print("O seu chute foi menor do que o número secreto!")
+
     rodada = rodada + 1
-  print('Fim de jogo!')
+
+print("Fim do jogo !")
